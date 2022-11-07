644154a1b1c40b3d19e60c64cae52dd4b6bd5d82
oshans <oshans@timbertruss.com.au>
2022-11-08 09:59:24 +1100

    first commit  - modified api controller

--- sampleDotNetCore/Controllers/WeatherForecastController.cs
+++ sampleDotNetCore/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace sampleDotNetCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching123"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
