84e89706cc2cd5edb84781079722c298b77fc946
oshans <oshans@timbertruss.com.au>
2022-11-08 11:26:04 +1100

    change 4

--- sampleDotNetCore/Controllers/WeatherForecastController.cs
+++ sampleDotNetCore/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace sampleDotNetCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1_1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
